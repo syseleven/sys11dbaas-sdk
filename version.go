1078197ca83a1611120d0cc5f7e23f352206b790
Tobias Kohlbau <t.kohlbau@syseleven.de>
2026-03-02 13:53:28 +0100

    feat: generate client sdk from openapi specification (#1)

--- version.go
+++
@@ -1,4 +0,0 @@
-package sys11dbaassdk
-
-const AppName, VersionMajor, VersionMinor, VersionPatch, VersionBuild string = "sys11dbaas-sdk", "0", "0", "1", "dev"
-const FullVersion string = AppName + "/" + VersionMajor + "." + VersionMinor + "." + VersionPatch + "-" + VersionBuild
